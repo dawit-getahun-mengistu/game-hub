425650e2171ec1f6cc14e9d2f6bb0d5d59498f9a
dawit-getahun-mengistu <112875081+dawit-getahun-mengistu@users.noreply.github.com>
2024-02-13 20:02:40 +0300

    get genres

--- src/components/GameGrid.tsx
+++ src/components/GameGrid.tsx
@@ -2,9 +2,10 @@ import { SimpleGrid, Text } from "@chakra-ui/react";
 import useGames from "../hooks/useGame";
 import GameCard from "./GameCard";
 import GameCardSkeleton from "./GameCardSkeleton";
+import GameCardContainer from "./GameCardContainer";
 
 const GameGrid = () => {
-  const { games, error, isloading } = useGames();
+  const { data, error, isloading } = useGames();
   const skeletons = [1, 2, 3, 4, 5, 6];
 
   return (
@@ -13,12 +14,20 @@ const GameGrid = () => {
 
       <SimpleGrid
         columns={{ sm: 1, md: 2, lg: 3, xl: 4 }}
-        spacing={10}
-        padding={10}
+        spacing={3}
+        padding="10px"
       >
         {isloading
-          ? skeletons.map((skeleton) => <GameCardSkeleton key={skeleton} />)
-          : games.map((game) => <GameCard key={game.id} game={game} />)}
+          ? skeletons.map((skeleton) => (
+              <GameCardContainer>
+                <GameCardSkeleton key={skeleton} />
+              </GameCardContainer>
+            ))
+          : data.map((data) => (
+              <GameCardContainer>
+                <GameCard key={data.id} game={data} />
+              </GameCardContainer>
+            ))}
       </SimpleGrid>
     </>
   );
---
+++ src/components/GenreList.tsx
@@ -0,0 +1,28 @@
+import { HStack, Image, List, ListItem, Spinner, Text } from "@chakra-ui/react";
+import useGenres from "../hooks/useGenres";
+
+const GenreList = () => {
+  const { data, isloading, error } = useGenres();
+
+  if (error) return null;
+  if (isloading) return <Spinner />;
+
+  return (
+    <List>
+      {data.map((genre) => (
+        <ListItem key={genre.id} paddingY={"5px"}>
+          <HStack>
+            <Image
+              boxSize="32px"
+              borderRadius={8}
+              src={genre.image_background}
+            />
+            <Text fontSize="lg">{genre.name}</Text>
+          </HStack>
+        </ListItem>
+      ))}
+    </List>
+  );
+};
+
+export default GenreList;
